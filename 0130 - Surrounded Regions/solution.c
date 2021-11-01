707159ca23e32f20df2249a8e3abde8b52a9694d
kevin1010607 <kevin.1010607@gmail.com>
2021-11-01 12:56:01 +0800

    update

---
+++ 0130 - Surrounded Regions/solution.c
@@ -0,0 +1,23 @@
+void solve(char** board, int boardSize, int* boardColSize){
+    int m = boardSize, n = boardColSize[0];
+    for(int i = 1; i < m-1; i++){
+        if(board[i][0] == 'O') dfs(i, 1, board, m, n);
+        if(board[i][n-1] == 'O') dfs(i, n-2, board, m, n);
+    }
+    for(int j = 1; j < n-1; j++){
+        if(board[0][j] == 'O') dfs(1, j, board, m, n);
+        if(board[m-1][j] == 'O') dfs(m-2, j, board, m, n);
+    }
+    for(int i = 1; i < m-1; i++){
+        for(int j = 1; j < n-1; j++){
+            if(board[i][j] == 'O') board[i][j] = 'X';
+            if(board[i][j] == '#') board[i][j] = 'O';
+        }
+    }
+}
+void dfs(int x, int y, char **board, int m, int n){
+    if(x<1||x>=m-1||y<1||y>=n-1||board[x][y]!='O') return;
+    board[x][y] = '#';
+    static int dx[] = {1,0,-1,0}, dy[] = {0,1,0,-1};
+    for(int i = 0; i < 4; i++) dfs(x+dx[i], y+dy[i], board, m, n);
+}
