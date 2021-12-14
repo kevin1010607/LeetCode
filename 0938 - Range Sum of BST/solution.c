78d55b12ed1df71e15f34598281afee00e3c21ab
kevin1010607 <kevin.1010607@gmail.com>
2021-12-14 10:45:58 +0800

    update

---
+++ 0938 - Range Sum of BST/solution.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int rangeSumBST(struct TreeNode* root, int low, int high){
+    if(!root) return 0;
+    if(root->val>=low && root->val<=high)
+        return root->val+rangeSumBST(root->left, low, high)+rangeSumBST(root->right, low, high);
+    else if(root->val < low)
+        return rangeSumBST(root->right, low, high);
+    else if(root->val > high)
+        return rangeSumBST(root->left, low, high);
+    return 0;
+}
