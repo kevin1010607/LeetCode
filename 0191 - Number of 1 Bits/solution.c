a389c561948f0a079ed48b8b575a5a2b20f77f69
kevin1010607 <kevin.1010607@gmail.com>
2022-05-26 16:25:04 +0800

    update on 2022/05/26

--- 0191 - Number of 1 Bits/solution.c
+++ 0191 - Number of 1 Bits/solution.c
@@ -1,8 +1,5 @@
 int hammingWeight(uint32_t n) {
     int res = 0;
-    while(n){
-        res += n&1;
-        n >>= 1;
-    }
+    while(n) n = n&(n-1), res++;
     return res;
 }
--- 0191 - Number of 1 Bits/solution.cpp
+++ 0191 - Number of 1 Bits/solution.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int res = 0;
-        while(n){
-            res += n&1;
-            n >>= 1;
-        }
+        while(n) n = n&(n-1), res++;
         return res;
     }
 };
